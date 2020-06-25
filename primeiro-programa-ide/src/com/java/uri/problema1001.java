f0132fb920a2b55f36820c285edbbc5c4a69f578
Marcio da Silva Eduardo <marcioeduardo.ti@gmail.com>
2020-06-25 15:52:46 -0300

    25.06.2020 - 15:52

---
+++ primeiro-programa-ide/src/com/java/uri/problema1001.java
@@ -0,0 +1,24 @@
+package com.java.uri;
+
+import java.util.Scanner;
+
+public class problema1001 {
+
+	public static void main(String[] args) {
+		/*
+		int a, b, x;
+		
+		Scanner sc = new Scanner(System.in);
+		
+		a = sc.nextInt();
+		b = sc.nextInt();
+		x = a + b;
+		
+		System.out.println(x);
+		*/
+		
+	
+		
+	}
+
+}
