661b43e2e2e20ab04921f802c9853e46c68e1e94
Marcio da Silva Eduardo <marcioeduardo.ti@gmail.com>
2020-06-25 15:19:37 -0300

    Atualização

--- primeiro-programa-ide/src/com/java/executavel/HelloWorld.java
+++ primeiro-programa-ide/src/com/java/executavel/HelloWorld.java
@@ -1,9 +1,9 @@
+package com.java.executavel;
 
 public class HelloWorld {
 
 	public static void main(String[] args) {
-		
-		System.out.println("Hello Word");
+		System.out.println("Hello World");
 
 	}
 
